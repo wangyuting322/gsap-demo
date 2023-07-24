6348da70579dd28ae8feb3db923c5c4930958d36
wangyuting322 <2428282291@qq.com>
2023-07-24 17:33:27 +0800

    feat -> 添加timeline；添加observer；添加svg

---
+++ src/views/TimelineView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="TimelineView flex-col">
+    <nav class="flex-row pointer">
+      <div
+        v-for="(item, index) in list"
+        :class="{ activeNav: index === activeIndex }"
+        :key="index"
+        :href="item.cmp.name"
+        @click="changeAcctive(index)"
+      >
+        {{ item.name }} |
+      </div>
+    </nav>
+    <section class="flex-col-all flex-col">
+      <component :is="list[activeIndex].cmp"></component>
+    </section>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import Timeline from '@/components/timeline/Timeline.vue'
+let list = [
+  { name: 'timeline', cmp: Timeline },
+]
+let activeIndex = ref(0)
+function changeAcctive(index) {
+  activeIndex.value = index
+}
+</script>
+<style lang="scss" scoped>
+.TimelineView{
+ 
+
+}
+</style>
